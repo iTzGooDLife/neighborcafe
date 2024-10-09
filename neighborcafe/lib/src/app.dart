479e50d7738d74432965e61ed5e403faf24d6f9b
Julio Amaru Cerda Villablanca <julio.cerdav@sansano.usm.cl>
2024-10-09 17:47:46 -0300

    feat: add colors and remove i18 support

--- neighborcafe/lib/src/app.dart
+++ neighborcafe/lib/src/app.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'sample_feature/sample_item_details_view.dart';
 import 'sample_feature/sample_item_list_view.dart';
 import 'settings/settings_controller.dart';
 import 'settings/settings_view.dart';
+import 'settings/app_colors.dart'; // Importa el archivo de colores
 
 /// The Widget that configures your application.
 class MyApp extends StatelessWidget {
@@ -35,14 +34,14 @@ class MyApp extends StatelessWidget {
           // Provide the generated AppLocalizations to the MaterialApp. This
           // allows descendant Widgets to display the correct translations
           // depending on the user's locale.
-          localizationsDelegates: const [
+          /* localizationsDelegates: const [
             AppLocalizations.delegate,
             GlobalMaterialLocalizations.delegate,
             GlobalWidgetsLocalizations.delegate,
             GlobalCupertinoLocalizations.delegate,
-          ],
+          ], */
           supportedLocales: const [
-            Locale('en', ''), // English, no country code
+            Locale('es', ''), // Español
           ],
 
           // Use AppLocalizations to configure the correct application title
@@ -50,15 +49,50 @@ class MyApp extends StatelessWidget {
           //
           // The appTitle is defined in .arb files found in the localization
           // directory.
-          onGenerateTitle: (BuildContext context) =>
-              AppLocalizations.of(context)!.appTitle,
+          onGenerateTitle: (BuildContext context) => 'Neighbor Café',
 
           // Define a light and dark color theme. Then, read the user's
           // preferred ThemeMode (light, dark, or system default) from the
           // SettingsController to display the correct theme.
-          theme: ThemeData(),
-          darkTheme: ThemeData.dark(),
+          /* theme: ThemeData(),
+          darkTheme: ThemeData.dark(), */
           themeMode: settingsController.themeMode,
+
+          theme: ThemeData(
+            primaryColor: AppColors.primaryColor,
+            scaffoldBackgroundColor: AppColors.backgroundColor,
+            appBarTheme: const AppBarTheme(
+              backgroundColor: AppColors.primaryColor,
+              titleTextStyle: TextStyle(color: Colors.white),
+            ),
+            textTheme: const TextTheme(
+              bodyMedium: TextStyle(
+                  color: AppColors
+                      .textColor), // Usa bodyMedium en lugar de bodyText1
+            ),
+            colorScheme: ColorScheme.fromSwatch().copyWith(
+              primary: AppColors.primaryColor,
+              secondary: AppColors.secondaryColor,
+            ),
+          ),
+
+          // Tema oscuro personalizado
+          darkTheme: ThemeData.dark().copyWith(
+            primaryColor: AppColors.primaryColor,
+            scaffoldBackgroundColor: Colors.black,
+            appBarTheme: const AppBarTheme(
+              backgroundColor: AppColors.primaryColor,
+              titleTextStyle: TextStyle(color: Colors.white),
+            ),
+            textTheme: const TextTheme(
+              bodyMedium: TextStyle(
+                  color: Colors.white), // Usa bodyMedium en lugar de bodyText1
+            ),
+            colorScheme: const ColorScheme.dark().copyWith(
+              primary: AppColors.primaryColor,
+              secondary: AppColors.secondaryColor,
+            ),
+          ),
 
           // Define a function to handle named routes in order to support
           // Flutter web url navigation and deep linking.
