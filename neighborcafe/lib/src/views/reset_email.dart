a67e0f1ad5c32404f3788665d1052ca0bb83af92
Julio Amaru Cerda Villablanca <julio.cerdav@sansano.usm.cl>
2024-10-15 01:32:14 -0300

    style: change title reset password

--- neighborcafe/lib/src/views/reset_email.dart
+++ neighborcafe/lib/src/views/reset_email.dart
@@ -39,7 +39,7 @@ class _PasswordResetViewState extends State<PasswordResetView> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             const Text(
-              'Iniciar Sesión',
+              'Recuperar Contraseña',
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 24,
