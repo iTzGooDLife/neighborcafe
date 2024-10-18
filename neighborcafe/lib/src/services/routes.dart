547e39af108724d9456a80ad32c7a54c0de63d3d
Julio Amaru Cerda Villablanca <julio.cerdav@sansano.usm.cl>
2024-10-18 17:57:33 -0300

    feat: add new route - email verification

--- neighborcafe/lib/src/services/routes.dart
+++ neighborcafe/lib/src/services/routes.dart
@@ -3,6 +3,8 @@ import '../settings/settings_controller.dart';
 import '../views/public/register_view.dart';
 import '../views/public/login_view.dart';
 import '../views/public/welcome_screen.dart';
+import '../views/public/reset_email.dart';
+import '../views/private/email_verification.dart';
 import '../views/private/home_screen.dart';
 import '../views/private/map_view.dart';
 import '../views/private/stores_view.dart';
@@ -10,20 +12,20 @@ import '../views/private/recommendations_view.dart';
 import '../views/private/review_store_view.dart';
 import '../views/private/initial_view.dart';
 import '../settings/settings_view.dart';
-import '../views/public/reset_email.dart';
 
 class AppRoutes {
   static const String welcome = '/';
   static const String login = '/login';
   static const String register = '/register';
   static const String home = '/home';
+  static const String resetpassword = '/reset_password';
   static const String map = '/map';
   static const String settings = '/settings';
   static const String stores = '/stores';
   static const String recommendationsview = '/recommendations';
   static const String initialview = '/initialview';
   static const String reviewstore = '/review_store';
-  static const String resetpassword = '/reset_password';
+  static const String checkemail = '/check_email';
 
   static Map<String, WidgetBuilder> routes(
           SettingsController settingsController) =>
@@ -39,6 +41,7 @@ class AppRoutes {
         initialview: (context) => const InitialView(),
         reviewstore: (context) => const ReviewStoreView(),
         resetpassword: (context) => const PasswordResetView(),
+        checkemail: (context) => const EmailVerificationScreen(),
       };
 
   static List<String> get protectedRoutes => [
@@ -49,5 +52,6 @@ class AppRoutes {
         recommendationsview,
         initialview,
         reviewstore,
+        checkemail,
       ];
 }
